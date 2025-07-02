d2db0be5135f5ea3c49a57a4cb8837be02cd9569
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-07-02 18:22:28 +0530

    Update start.py

--- SONALI/utils/inline/start.py
+++ SONALI/utils/inline/start.py
@@ -26,9 +26,9 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
+            InlineKeyboardButton(text=_["S_B_7"], url="https://files.catbox.moe/fe22ou.jpg"),
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], url="https://files.catbox.moe/fe22ou.jpg")],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
     return buttons
